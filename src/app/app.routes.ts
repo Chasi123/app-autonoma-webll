21d96520b45900d29234f9a37a2f62405027ec26
chasi123 <christopherchasiloa56@gmail.com>
2025-08-06 14:51:41 -0500

    Ayudaaaaaaaaa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home';
+import { LoginComponent } from './components/login/login';
+import { RegistroComponent } from './components/registro/registro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'registro', component: RegistroComponent},
+
+];
